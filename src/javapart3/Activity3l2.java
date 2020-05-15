3fbcfcff0766d94fb76b43f1703240d904e68efd
pcongviet <64540537+pcongviet@users.noreply.github.com>
2020-05-15 14:33:10 +0700

    mang

--- src/javapart3/Activity3l2.java
+++ src/javapart3/Activity3l2.java
@@ -3,16 +3,15 @@ package javapart3;
 public class Activity3l2 {
     public static void main(String[] args) {
         int [] a = {20,20,30,40,50,50,50};
-        int [] b = new int[a.length] ;
-        for ( int i =0; i < a.length; i++ ) {
-            int c= 0;
-            for (int j=0; j < a.length; j++) {
-                if (a[i] != a[j]) {
-                    a[c] = a[i];
-                    c++;
-                }
+        int [] b = new int[4] ;
+        int j= 0;
+        for ( int i =0;i<a.length-1  ; i++ ) {
+            if(a[i]!=a[i+1]){
+                b[j]=a[i];j++;
             }
         }
+        b[j]=a[a.length-1];
+
         for(int element:b){
             System.out.println(element);
         }
