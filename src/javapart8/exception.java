62a27b67f4958872d0fcb1869d7da8c76e7778d6
pcongviet <64540537+pcongviet@users.noreply.github.com>
2020-07-17 16:33:30 +0700

    xu li ngoai le

---
+++ src/javapart8/exception.java
@@ -0,0 +1,29 @@
+package javapart8;
+
+public class exception {
+    public static void main (String[] args){
+        try{
+        int arr[] = new int[5];
+        arr[5] = 4;
+        System.out.println("arr[5] = " + arr[5]);}
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e);
+        }
+        try{
+        int zero = 0;
+        int average = 10 / zero;
+        System.out.println("Average = " + average);
+        }
+        catch (ArithmeticException e){
+            System.out.println(e);
+        }
+        try {
+        String obj = null;
+        System.out.println(obj.length());}
+        catch (NullPointerException e){
+            System.out.println(e);
+        }
+
+        System.out.println("Finished!");
+    }
+}
